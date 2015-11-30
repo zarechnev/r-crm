0b5737eebfc6945efcff13e2dd80eb63dce2b4c8
Ruslan A. Zarechnev <zarechnev.r@yandex.ru>
2015-11-30 12:27:38 +0600

    Изменено окно ввода логина и пароля.

--- rcrm_django/rcrm_django/views.py
+++ rcrm_django/rcrm_django/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.contrib import auth
 
 
+@login_required(login_url='/auth/login')
 def index(request):
     args = {}
     args['username'] = auth.get_user(request).username
