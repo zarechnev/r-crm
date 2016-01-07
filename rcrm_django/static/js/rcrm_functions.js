a74101fcd37b3e4396913f19d1f2128114338ad3
Ruslan A. Zarechnev <zarechnev.r@yandex.ru>
2016-01-07 22:17:38 +0600

    Подправил оформление рааздела задачи.
    *) Зависимые переключатели теперь вертикальны во всех разделах.
    *) При наведении курсора на логин - появляется имя и фамилия.

--- rcrm_django/static/js/rcrm_functions.js
+++ rcrm_django/static/js/rcrm_functions.js
@@ -32,7 +32,7 @@ function show_crm(url)
                         }
                     );
                 });
-                $(function() {$( ".cl_radio_btns" ).buttonset();});
+                $(function() {$( ".cl_radio_btns" ).buttonsetv();});
             }
         });
     }
@@ -47,7 +47,7 @@ function show_clients()
             cache: false,
             success:function(html){
                 $("#clients_content").html(html);
-                $(function() {$( ".cl_radio_btns" ).buttonset();});
+                $(function() {$( ".cl_radio_btns" ).buttonsetv();});
             }
         });
     }
@@ -62,7 +62,7 @@ function show_users()
             cache: false,
             success:function(html){
                 $("#users_content").html(html);
-                $(function() {$( ".cl_radio_btns" ).buttonset();});
+                $(function() {$( ".cl_radio_btns" ).buttonsetv();});
             }
         });
     }
@@ -336,3 +336,21 @@ function add_message()
         get_chat();
         $("#id_message").val("");
     }
+
+(function( $ ){
+    //plugin buttonset vertical
+    $.fn.buttonsetv = function() {
+      $(':radio, :checkbox', this).wrap('<div style="margin: 1px"/>');
+      $(this).buttonset();
+      $('label:first', this).removeClass('ui-corner-left').addClass('ui-corner-top');
+      $('label:last', this).removeClass('ui-corner-right').addClass('ui-corner-bottom');
+      mw = 0; // max witdh
+      $('label', this).each(function(index){
+         w = $(this).width();
+         if (w > mw) mw = w;
+      })
+      $('label', this).each(function(index){
+        $(this).width(mw);
+      })
+    };
+})( jQuery );
