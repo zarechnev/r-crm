447d739c64b53c7dc27bc872b0759f508ad398fb
Ruslan A. Zarechnev <zarechnev.r@yandex.ru>
2016-04-27 16:22:15 +0700

    Translating.....translating...

--- rcrm_django/clients/models.py
+++ rcrm_django/clients/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 class Client(models.Model):
@@ -9,13 +10,15 @@ class Client(models.Model):
         ('400', 'Средний'),
         ('500', 'Низкий'),
     )
-    name = models.CharField(max_length=200, unique=True, blank=False, null=False, verbose_name="Полное название организации")
-    sname = models.CharField(max_length=100, unique=True, blank=False, null=False, verbose_name="Краткое название организации")
-    inn = models.CharField(max_length=15, unique=True, blank=False, null=False, verbose_name="ИНН")
-    address = models.CharField(max_length=300, blank=False, null=False, verbose_name="Адрес организации")
-    phone = models.CharField(max_length=50, blank=False, null=False, verbose_name="Номер телефона организации")
-    email = models.EmailField(unique=True, blank=False, null=False, verbose_name="Электронная почта организации")
-    priority = models.IntegerField(choices=PRIORITY_VALUES, verbose_name="Приоритет клиента")
-    create_date = models.DateTimeField(verbose_name="Дата регистрации организации")
-    is_active = models.BooleanField(default=True, blank=False, verbose_name="Обслуживается")
-    is_deleted = models.BooleanField(default=False, verbose_name="Удален")
+    name = models.CharField(max_length=200, unique=True, blank=False, null=False,
+                            verbose_name=_("Organization full name"))
+    sname = models.CharField(max_length=100, unique=True, blank=False, null=False,
+                             verbose_name=_("Organization short name"))
+    inn = models.CharField(max_length=15, unique=True, blank=False, null=False, verbose_name=_("Organization ID"))
+    address = models.CharField(max_length=300, blank=False, null=False, verbose_name=_("Organization address"))
+    phone = models.CharField(max_length=50, blank=False, null=False, verbose_name=_("Organization phone number"))
+    email = models.EmailField(unique=True, blank=False, null=False, verbose_name=_("Organization e-mail"))
+    priority = models.IntegerField(choices=PRIORITY_VALUES, verbose_name=_("Organization priority"))
+    create_date = models.DateTimeField(verbose_name=_("Organization registration date"))
+    is_active = models.BooleanField(default=True, blank=False, verbose_name=_("Is enabled"))
+    is_deleted = models.BooleanField(default=False, verbose_name=_("Is deleted"))
